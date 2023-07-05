074c0f7425b6f840be53e7c31860605eb9ff1f93
Vaibhav Joshi <96672994+VaibhavJoshi95@users.noreply.github.com>
2023-07-05 10:40:47 +0530

    Data_transformation added

---
+++ src/components/data_transformation.py
@@ -0,0 +1,122 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+from src.utils import save_object
+
+@dataclass
+class DataTransformationconfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+
+class DataTRansformation:
+    def __init__(self):
+        self.data_tranfomation_cofig= DataTransformationconfig()
+
+    def get_data_transformation_object(self):
+        try:
+            logging.info('Data Transformation Initiated')
+
+            #Define whic columns should be ordinal-encoded and which shoul be scaled
+
+            categorical_cols= ['sex','smoker','region']
+            numerical_cols=['age', 'bmi','children']
+
+            #Define the custom ranking for each ordinal variable
+
+            sex_categories=['male','female']
+            smoker_categories=['yes','no']
+            region_categories=['southwest', 'southeast', 'northwest', 'northeast']
+
+            logging.info('Pipleine Initiated')
+
+            ## Numerical Pipeline
+            num_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='median')),
+                ('scaler',StandardScaler())
+
+                ]
+
+            )
+
+            # Categorigal Pipeline
+            cat_pipeline=Pipeline(
+                steps=[
+                ('imputer',SimpleImputer(strategy='most_frequent')),
+                ('ordinalencoder',OrdinalEncoder(categories=[sex_categories,smoker_categories,region_categories])),
+                ('scaler',StandardScaler())
+                ]
+
+            )
+
+            preprocessor=ColumnTransformer([
+            ('num_pipeline',num_pipeline,numerical_cols),
+            ('cat_pipeline',cat_pipeline,categorical_cols)
+            ])
+
+            return preprocessor
+        
+            logging.info('Pipeline Completed')
+
+        except Exception as e:
+            logging.info("Error in Data Transformation")
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
+        try:
+            #Reading train and test data
+            train_df = pd.read_csv(train_path)
+            test_df= pd.read_csv(test_path)
+            logging.info("Read train ans test data completed")
+            logging.info(f'Train Dataframe Head:\n {train_df.head().to_string()}')
+            logging.info(f'Test Dataframe Head:\n {test_df.head().to_string()}')
+
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj= self.get_data_transformation_object()
+
+            target_column_name='expenses'
+            drop_columns=[target_column_name]
+            input_feature_train_df=train_df.drop(columns=drop_columns,axis=1)
+            target_feature_train_df=train_df[target_column_name]
+            input_feature_test_df= test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            ##Transforming using preprocessor obj
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr= preprocessing_obj.transform(input_feature_test_df)
+            logging.info('Applying preprocessing object on training and testing datasets')
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+
+            logging.info('Preprocessor pickle file saved')
+
+            return(
+                train_arr,
+                test_arr,
+                self.data_tranfomation_cofig.preprocessor_obj_file_path,
+            )
+
+
+        except Exception as e:
+            logging.info('Exception occured in the inititiate_data_transformation')
+            raise CustomException(e,sys)
