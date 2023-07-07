54da9d7f9d8b2ae21276490f3324c1ef1ced4541
Vaibhav Joshi <96672994+VaibhavJoshi95@users.noreply.github.com>
2023-07-07 10:41:45 +0530

    Updated

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,7 +1,7 @@
 import os
 import sys
-from src.logger import logging
 from src.exception import CustomException
+from src.logger import logging
 import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
